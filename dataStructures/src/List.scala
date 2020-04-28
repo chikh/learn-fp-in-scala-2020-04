96515a5bb662a08d374d0e3ee91327f9dedbfa95
Georgy Chikhladze <georgiy.chikhladze@gmail.com>
2020-04-28 14:01:29 +0200

    Finilize List excercises

--- dataStructures/src/List.scala
+++ dataStructures/src/List.scala
@@ -12,6 +12,9 @@ object List {
     if (xs.isEmpty) Nil
     else Cons(xs.head, apply(xs.tail: _*))
 
+  def length(as: List[_]) =
+    foldLeft(as)(0)((b, _) => b + 1)
+
   @tailrec
   def drop[A](l: List[A], n: Int): List[A] =
     if (n < 1) l
@@ -37,4 +40,35 @@ object List {
 
   def foldRightViaLeft[A, B](l: List[A])(z: B)(op: (A, B) => B): B =
     (((r: List[A]) => foldLeft(r)(z)((b, a) => op(a, b))) compose reverse[A])(l)
+
+  def append[A](l1: List[A])(l2: List[A]): List[A] =
+    foldRightViaLeft(l1)(l2)(Cons(_, _))
+
+  def concat[A](ls: List[List[A]]): List[A] =
+    foldRightViaLeft(ls)(Nil: List[A])(append(_)(_))
+
+  def map[A, B](as: List[A])(f: A => B): List[B] =
+    foldRightViaLeft(as)(Nil: List[B])((a, b) => Cons(f(a), b))
+
+  def filter[A](as: List[A])(f: A => Boolean): List[A] =
+    flatMap(as)(a => if (f(a)) List(a) else Nil)
+    // foldRightViaLeft(as)(Nil: List[A])((a, b) => if (f(a)) Cons(a, b) else b)
+
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] =
+    foldRightViaLeft(as)(Nil: List[B])((a, b) => append(f(a))(b))
+
+  def zipWith[A](as1: List[A])(as2: List[A])(f: (A, A) => A): List[A] =
+    as1 match {
+      case Nil => Nil
+      case Cons(h1, t1) => as2 match {
+        case Nil => Nil
+        case Cons(h2, t2) => Cons(f(h1, h2), zipWith(t1)(t2)(f))
+      }
+    }
+
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = sup match {
+    case l if (l == sub) => true
+    case Nil => false
+    case Cons(h, t) => hasSubsequence(t, sub) || hasSubsequence(reverse(t), reverse(sub))
+  }
 }
--- dataStructures/test/src/ListTests.scala
+++ dataStructures/test/src/ListTests.scala
@@ -5,6 +5,12 @@ import List._
 
 object ListTests extends TestSuite {
   def tests = Tests {
+    test("length") {
+      val r = length(List(1, 2, 3))
+      assert(r == 3)
+      r
+    }
+
     test("drop") {
       test("one makes empty") {
         val result = drop(Cons(1, Nil), 1)
@@ -63,6 +69,82 @@ object ListTests extends TestSuite {
       val r = reverse(List(1, 2, 3))
       assert(r == List(3, 2, 1))
       r
+    }
+
+    test("append") {
+      val r = append(List(1, 2))(List(3, 4))
+      assert(r == List(1, 2, 3, 4))
+      r
+    }
+
+    test("concat") {
+      test("should do simple concat") {
+        val r = concat(List(List(1, 2), List(3, 4)))
+        assert(r == List(1, 2, 3, 4))
+        r
+      }
+
+      /* TODO test("should be linear to the amount of elements") { ??? } */
+    }
+
+    test("map") {
+      val r = map(List(1, 2, 3))(_.toString)
+      assert(r == List("1", "2", "3"))
+      r
+    }
+
+    test("filter") {
+      var r = filter(List(1, 2, 3, 4))(_ % 2 == 0)
+      assert(r == List(2, 4))
+      r
+    }
+
+    test("flatMap") {
+      var r = flatMap(List(1, 2, 3))(i => List(i, i))
+      assert(r == List(1, 1, 2, 2, 3, 3))
+      r
+    }
+
+    test("zipWith") {
+      test("general case") {
+        val r = zipWith(List(1, 2))(List(3, 4))(_ + _)
+        assert(r == List(4, 6))
+        r
+      }
+
+      test("the result length should be first list length if it's shorter") {
+        val first = List(1)
+        val r = length(zipWith(first)(List(2, 3))(_ + _))
+        assert(r == length(first))
+        r
+      }
+
+      test("the result length should be second list length if it's shorter") {
+        val second = List(1)
+        val r = length(zipWith(List(2, 3))(second)(_ + _))
+        assert(r == length(second))
+        r
+      }
+
+      test("with Nil should be Nil") {
+        var r = zipWith(Nil: List[Int])(Nil: List[Int])(_ + _)
+        assert(r == Nil)
+        r
+      }
+    }
+
+    test("hasSubsequence") {
+      test("true") {
+        var r = hasSubsequence(List(1, 2, 3, 4), List(2, 3))
+        assert(r == true)
+        r
+      }
+
+      test("false") {
+        var r = hasSubsequence(List(1, 2, 3, 4), List(1, 3))
+        assert(r == false)
+        r
+      }
     }
   }
 }
