7b626d4fb7f33af6ddf128669fc85be08b33d6f9
Georgy Chikhladze <georgiy.chikhladze@gmail.com>
2022-07-23 21:49:32 +0200

    Re-implement Par inspired by the book

--- parallel/src/Par.scala
+++ parallel/src/Par.scala
@@ -1,37 +1,63 @@
 package parallel
 
-import java.util.concurrent.Callable
 import java.util.concurrent.ExecutorService
-
-case class Par[A](a: () => A, forked: Boolean)
+import java.util.concurrent.Future
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.Callable
 
 object Par {
-  def unit[A](a: => A): Par[A] = Par(() => a, false)
+  type Par[A] = ExecutorService => Future[A]
+
+  def unit[A](a: A): Par[A] = _ => UnitFuture(a)
 
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 
-  def fork[A](pa: Par[A]): Par[A] = pa match {
-    case Par(a, false) => Par(a, true)
-    case p => p
+  def fork[A](pa: Par[A]): Par[A] =
+    es =>
+      es.submit(new Callable[A] {
+        override def call: A = pa(es).get // TODO: don't waste threads
+      })
+
+  def run[A](es: ExecutorService)(p: Par[A]): Future[A] = p(es)
+
+  def map2[A, B, C](pa: Par[A], pb: Par[B])(f: (A, B) => C): Par[C] =
+    es => {
+      UnitFuture(
+        f(pa.run(es).get, pb.run(es).get) // TODO: get rid of all the blocking
+      )
+    }
+
+  def asyncF[A, B](f: A => B): A => Par[B] =
+    a => lazyUnit(f(a))
+
+  implicit class ParOps[A](pa: Par[A]) {
+    def run(es: ExecutorService): Future[A] = Par.run(es)(pa)
+    def map2[B, C](pb: Par[B])(f: (A, B) => C): Par[C] = Par.map2(pa, pb)(f)
   }
 
-  def run[A](p: Par[A])(implicit es: ExecutorService): A = p match {
-    case Par(a, false) => a()
-    case Par(a, true) => es.submit(new Callable[A] {
-      override def call: A = a()
-    }).get() // TODO: don't block ("sum in parallel" test should work even having < 3 threads in a pool)
-  }
+}
 
-  // TODO: implement without having to provide ExecutorService
-  def map2[A, B, C](pa: Par[A], pb: Par[B])(f: (A, B) => C)(implicit es: ExecutorService): Par[C] =
-    unit(f(run(pa), run(pb)))
+object ExampleParUsage {
+  import Par._
 
-  // TODO: implement without having to provide ExecutorService
-  def parSum(s: IndexedSeq[Int])(implicit es: ExecutorService): Par[Int] = {
+  def parSum(s: IndexedSeq[Int]): Par[Int] = {
     if (s.length <= 1) unit(s.headOption.getOrElse(0))
     else {
       val (l, r) = s.splitAt(s.length / 2)
-      map2(fork(parSum(l)), fork(parSum(r)))(_ + _)
+      fork(parSum(l)).map2(fork(parSum(r)))(_ + _)
     }
   }
+}
+
+case class UnitFuture[A](a: A) extends Future[A] {
+  def cancel(x: Boolean): Boolean = false
+
+  def isCancelled(): Boolean = false
+
+  def isDone(): Boolean = true
+
+  def get(): A = a
+
+  def get(x: Long, y: TimeUnit): A = a
+
 }
--- parallel/test/src/ParTests.scala
+++ parallel/test/src/ParTests.scala
@@ -2,30 +2,31 @@ package parallel
 
 import utest._
 import Par._
-import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 object ParTests extends TestSuite {
-  implicit val es: ExecutorService = Executors.newFixedThreadPool(3)
-
   def tests = Tests {
-    test("sum in parallel") {
+    test("ex usage: sum in parallel") {
       val s = Vector(1, 2, 3, 4, 5)
 
-      val r = run(parSum(s))
+      val r = ExampleParUsage
+        .parSum(s)
+        .run(Executors.newFixedThreadPool(3))
+        .get
+
       test("result is the same as normal sum") {
         assert(r == s.sum)
         r
       }
     }
-    
+
     test("map2 is lazy") {
       @volatile
       var cnt = 0
 
-      val p = Par(() => { cnt += 1; cnt }, false)
+      val p: Par[Int] = _ => { cnt += 1; UnitFuture(cnt) }
 
-      map2(p, p)(_ + _)
+      p.map2(p)(_ + _)
 
       assert(cnt == 0)
       cnt
